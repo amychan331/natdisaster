f53118c160d1b5bfb0b23dd800629ad7937b7078
amychan331@yahoo.com <amychan331@yahoo.com>
2018-07-28 21:54:22 -0700

    getting tthe sidebar and marker to work together

--- public/index.js
+++ public/index.js
@@ -2,6 +2,14 @@ require('dotenv').config()
 var geojson = require('./location.geojson')
 
 window.onload = function () {
+  fetch('https://desolate-crag-37715.herokuapp.com/London')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      console.log(myJson);
+    });
+
   mapboxgl.accessToken = process.env.MAPBOXGL_TOKEN;
 
   var map = new mapboxgl.Map({
@@ -12,34 +20,99 @@ window.onload = function () {
   })
 
   map.on('load', function () {
-    map.addSource('trees', {
-      type: 'geojson',
-      data: geojson
+    map.addLayer({
+      id: 'locations',
+      type: 'symbol',
+      source: {
+        type: 'geojson',
+        data: geojson
+      },
+      layout: {
+        'icon-image': 'marker-24',
+        'icon-allow-overlap': true,
+      }
     });
-    // Add marker/pointer
-    geojson.features.forEach(function(marker) {
-      // create a HTML element for each feature
-      var el = document.createElement('div');
-      el.className = 'marker';
 
-      // make a marker for each feature and add to the map
-      new mapboxgl.Marker(el)
-        .setLngLat(marker.geometry.coordinates)
-        .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-        .setHTML('<p>' + marker.properties.address + '</p>'))
-        .addTo(map);
-    });
+    // add sidebar
+    buildLocationList(geojson)
   })
+
+  function buildLocationList(incidents) {
+    for (i = 0; i < incidents.features.length; i++) {
+      // Building the listing HTML
+      var prop = incidents.features[i].properties;
+      var listings = document.getElementById('listings');
+      var listing = listings.appendChild(document.createElement('div'));
+      listing.className = 'item';
+      listing.id = 'listing-' + i;
+
+      var link = listing.appendChild(document.createElement('a'));
+      link.href = '#';
+      link.className = 'handle';
+      link.dataPosition = i;
+      link.innerHTML = prop.screen_name;
+
+      var content = listing.appendChild(document.createElement('div'));
+      content.innerHTML = prop.screen_name;
+
+      // Adding event listening for when sidebar link is clicked
+      link.addEventListener('click', function(e) {
+        var clickedListing = geojson.features[this.dataPosition];
+
+        flyToIncident(clickedListing);
+        createPopUp(clickedListing);
+
+        var activeItem = document.getElementsByClassName('active');
+        if (activeItem[0]) {
+          activeItem[0].classList.remove('active');
+        }
+        this.parentNode.classList.add('active');
+      });
+    }
+  }
 
   // INTERACTIVE FEATURES
+  function flyToIncident(currentFeature) {
+    console.log(currentFeature)
+    map.flyTo({
+      center: currentFeature.geometry.coordinates,
+      zoom: 15
+    });
+  }
+
+  function createPopUp(currentFeature) {
+    var popUps = document.getElementsByClassName('mapboxgl-popup');
+    if (popUps[0]) popUps[0].remove();
+
+    var popup = new mapboxgl.Popup({ closeOnClick: false })
+      .setLngLat(currentFeature.geometry.coordinates)
+      .setHTML('<p>' + currentFeature.properties.message + '</p>')
+      .addTo(map);
+  }
+
+  // When map is clicked
   map.on('click', function(e) {
-    // Query all the rendered points in the view
-    const features = map.queryRenderedFeatures(e.point, { layers: ['locations'] });
+    var features = map.queryRenderedFeatures(e.point, { layers: ['locations'] });
+    console.log(features)
     if (features.length) {
-      map.flyTo({
-        center: features[0].geometry.coordinates,
-        zoom: 15
-      })
+      var clickedPoint = features[0];
+
+      flyToIncident(clickedPoint);
+      createPopUp(clickedPoint);
+
+      var activeItem = document.getElementsByClassName('active');
+      if (activeItem[0]) {
+        activeItem[0].classList.remove('active');
+      }
+
+      var selectedFeature = clickedPoint.properties.screen_name;
+      for (var i = 0; i < stores.features.length; i++) {
+        if (stores.features[i].properties.handle === selectedFeature) {
+          selectedFeatureIndex = i;
+        }
+      }
+      var listing = document.getElementById('listing-' + selectedFeatureIndex);
+      listing.classList.add('active');
     }
-  })
+  });
 }
