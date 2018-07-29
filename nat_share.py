03812fc0048d075673cdc27660f46055051bc70c
Neil Vodoor <19353141+nvodoor@users.noreply.github.com>
2018-07-28 17:52:59 -0700

    Add files via upload

--- nat_share.py
+++ nat_share.py
@@ -1,26 +1,53 @@
 import tweepy
-import requests
+import json
+import geocoder
+
+g = geocoder.google('Shasta')
+latlng = g.latlng
 
 class SearchTweet:
 
-    def __init__(self, c_key, c_secret, at_token, at_secret):
-        self.auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-        self.auth.set_access_token(access_token, access_token_secret)
+    def __init__(self, c_key, c_secret, at_token, at_secret, location):
+        self.auth = tweepy.OAuthHandler(c_key, c_secret)
+        self.auth.set_access_token(at_token, at_secret)
         self.api = tweepy.API(self.auth, parser=tweepy.parsers.JSONParser())
+        self.location = geocoder.google(location)
+        self.latlng = self.location.latlng
+        self.user_info = []
 
-    def query(self, geo=''):
-        query = self.api.search(q='Shasta', lang='en', rpp=10, count=20, geocode=geo)
-        screen_names = []
+    def query(self):
+        geo=f"{self.latlng[0]},{self.latlng[1]},20mi"
+        query = self.api.search(q='*', lang='en', rpp=10, count=20, geocode=geo)
         for quer in query['statuses']:
-            screen_names.append(quer['user']['screen_name'])
-        return screen_names
+            coord = {
+                'screen_name': '',
+                'location': [],
+                'text': ''
+            }
+            coord['screen_name'] = quer['user']['screen_name']
+            loc = geocoder.google(quer['user']['location'])
+            loc = loc.latlng
+            if loc == None:
+                loc = self.latlng
+            coord['location'].append(loc[0])
+            coord['location'].append(loc[1])
+            coord['text'] = (quer['text'])
+            self.user_info.append(coord)
+        return self.user_info
 
-consumer_key = ''
-consumer_secret = ''
+    def printNames(self):
+        for info in self.user_info:
+            print(info)
 
-access_token = ''
-access_token_secret = ''
+# consumer_key = ''
+# consumer_secret = ''
 
-search = SearchTweet(consumer_key,consumer_secret,access_token,access_token_secret)
+# access_token = ''
+# access_token_secret = ''
 
-print(search.query())
+# search = SearchTweet(consumer_key,consumer_secret,access_token,access_token_secret, "Shasta")
+
+# print(search.query())
+
+# search.printNames()
+
